c3842c1174e17a4cccd5431f9029ec564b1773ea
Muhammadqodir-st <muhammadqodirov3609@gmail.com>
2026-01-16 15:21:56 +0500

    secound commit

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,23 @@
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from 'generated/prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
+    constructor() {
+        const adapter = new PrismaPg({
+            connectionString: process.env.DATABASE_URL
+        })
+
+        super({ adapter })
+    }
+
+    async onModuleInit() {
+        try {
+            await this.$connect()
+            console.log("Databace connect")
+        } catch (error) {
+            console.log(error, "Databace error")
+        }
+    }
+}
