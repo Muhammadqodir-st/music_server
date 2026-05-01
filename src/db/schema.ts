815c97c1895f5822704b617c93a0a705f4ec042e
Muhammadqodir-st <muhammadqodirov3609@gmail.com>
2026-05-01 07:46:40 +0500

    fix

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,13 +1,24 @@
-import { pgEnum, pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
+import { pgEnum, pgTable, text, timestamp, uuid, uniqueIndex, index } from "drizzle-orm/pg-core";
 
-export const providerEnum = pgEnum("provider", ["email", "google"]);
+export const ProviderEnum = pgEnum("provider", ["email", "google"]);
 
 export const usersTable = pgTable("users", {
     id: uuid().primaryKey().defaultRandom(),
     name: text("name").notNull(),
     email: text("email").notNull().unique(),
     avatar: text("avatar"),
-    provider: providerEnum("provider").notNull().default("email"),
+    provider: ProviderEnum("provider").notNull().default("email"),
     googleId: text("google_id"),
+    createdAt: timestamp("createdAt").notNull().defaultNow()
+}, (t) => ({
+    googleIdUnique: uniqueIndex("users_google_id_unique").on(t.googleId),
+    emailIdx: index("users_email_idx").on(t.email)
+}));
+
+export const musicTable = pgTable("musics", {
+    id: uuid().primaryKey().defaultRandom(),
+    title: text("title").notNull(),
+    artwork: text("artwork"),
+    song: text("song").notNull(),
     createdAt: timestamp("createdAt").notNull().defaultNow()
 });
