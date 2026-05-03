453711e3c1e78fc9cef2128742bbaf0d14d2901b
Muhammadqodir-st <muhammadqodirov3609@gmail.com>
2026-05-03 13:46:18 +0500

    aws

--- src/aws/aws.service.ts
+++ src/aws/aws.service.ts
@@ -1,4 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { S3Client } from '@aws-sdk/client-s3';
+import { Injectable, Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
-export class AwsService {}
+export class AwsService {
+    private readonly s3: S3Client;
+    private readonly logger = new Logger(AwsService.name)
+
+    private readonly region: string;
+    private readonly bucket: string;
+
+    constructor(private readonly config: ConfigService) {
+        const accessKeyId = this.config.get<string>('AWS_ACCESS_KEY_ID');
+        const secretAccessKey = this.config.get<string>('AWS_SECRET_ACCESS_KEY');
+        this.region = this.config.get<string>('AWS_REGION') ?? '';
+        this.bucket = this.config.get<string>('AWS_BUCKET') ?? '';
+
+        const missing = [
+            !accessKeyId && 'AWS_ACCESS_KEY_ID',
+            !secretAccessKey && 'AWS_SECRET_ACCESS_KEY',
+            !this.region && 'AWS_REGION',
+            !this.bucket && 'AWS_BUCKET'
+        ].filter(Boolean);
+
+        if (missing.length) {
+            this.logger.error(`AWS configuration is missing: ${missing.join(', ')}`);
+            throw new Error('AWS configuration is incomplate');
+        };
+
+        this.s3 = new S3Client({
+            region: this.region,
+            credentials: { accessKeyId: accessKeyId!, secretAccessKey: secretAccessKey! }
+        });
+    };
+}
